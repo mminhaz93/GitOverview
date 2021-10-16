b83b978b884e2a3adeb8091543d9471d3eac569b
Hasan <hasanmohammed100@gmail.com>
2021-10-15 22:22:17 -0400

    first commit

---
+++ src/JavaMod.java
@@ -0,0 +1,86 @@
+public class JavaMod {
+
+
+    /*
+    Access modifiers are object-oriented programming that is used to set the accessibility of classes, constructors, methods,
+
+    and other members of Java. Using the access modifiers we can set the scope or accessibility of these classes, methods, constructors, and other members
+
+Non-access modifiers : In java, we have 7 non-access modifiers. They are used with classes, methods, variables, constructors etc to provide information about their behavior to JVM.They are
+static
+final
+abstract
+synchronized
+transient
+volatile
+native
+
+Basic Operator-
+The Arithmetic Operators-An operator that performs arithmetic operations on groups and numbers.
+like + - * / % ++ --
+
+The Relational Operators-A relational operator is a programming language construct or operator that tests or defines some kind of relation between two entities.
+
+like  ==(equal to) !=(not equal to) >(greater than) <(less than)
+>=(greater than or equal to) <=(less than equal to)
+
+Bitwise operators are used to performing manipulation of individual bits of a number.
+& (bitwise and) | (bitwise or) ^ (bitwise XOR)~ (bitwise compliment)
+<< (left shift) >> (right shift) >>> (zero fill right shift)
+
+A logical operator is a symbol or word used to connect two or more expressions such that the value of the compound expression produced depends only on that of the original expressions and on the meaning of the operator.
+&& (logical and) || (logical or)! (logical not)
+
+Assignment operators are used in Java to assign values to variables.
+= += -= *= /= %= <<= >>= &= ^= l=
+Java's miscellaneous operators are: ternary operator, member access, comma, array index, new, instanceof, and typecast.
+
+wrapper classes (Integer, Long, Byte, Double, Float, Short) are subclasses of the abstract class Number.
+Wrapper object will be converted back to a primitive data type, and this process is called unboxing
+
+Number Methods
+xxxValue() Converts the value of this Number object to the xxx data type and returns it.
+compareTo() Compares this Number object to the argument.
+equals() Determines whether this number object is equal to the argument.
+valueOf() Returns an Integer object holding the value of the specified primitive.
+toString() Returns a String object representing the value of a specified int or Integer.
+parseInt() This method is used to get the primitive data type of a certain String.
+abs() Returns the absolute value of the argument.
+ceil() Returns the smallest integer that is greater than or equal to the argument. Returned as a double.
+floor() Returns the largest integer that is less than or equal to the argument. Returned as a double.
+rint() Returns the integer that is closest in value to the argument. Returned as a double.
+round() Returns the closest long or int, as indicated by the method's return type to the argument.
+min() Returns the smaller of the two arguments.
+max() Returns the larger of the two arguments.
+exp() Returns the base of the natural logarithms, e, to the power of the argument.
+log() Returns the natural logarithm of the argument.
+pow() Returns the value of the first argument raised to the power of the second argument.
+sqrt() Returns the square root of the argument.
+sin() Returns the sine of the specified double value.
+cos() Returns the cosine of the specified double value.
+tan()Returns the tangent of the specified double value.
+asin() Returns the arcsine of the specified double value.
+acos() Returns the arccosine of the specified double value.
+atan() Returns the arctangent of the specified double value.
+atan2() Converts rectangular coordinates (x, y) to polar coordinate (r, theta) and returns theta.
+toDegrees() Converts the argument to degrees.
+toRadians() Converts the argument to radians.
+random() Returns a random number.
+
+Escape Squence- Escape sequences are used to signal an alternative interpretation of a series of characters
+\t \b \n \r \f \' \"
+
+character method-
+isLetter() Determines whether the specified char value is a letter.
+isDigit() Determines whether the specified char value is a digit.
+isWhitespace() Determines whether the specified char value is white space.
+isUpperCase() Determines whether the specified char value is uppercase.
+isLowerCase() Determines whether the specified char value is lowercase.
+toUpperCase() Returns the uppercase form of the specified char value.
+toLowerCase() Returns the lowercase form of the specified char value.
+toString() Returns a String object representing the specified character value that is, a one-character string.
+
+string length- method returns the number of characters written in the String
+
+     */
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,160 @@
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String args[]) {
+
+        // xxxValue
+        Integer y = 5;
+
+        // Returns byte primitive data type
+        System.out.println(y.byteValue());
+
+        // Returns double primitive data type
+        System.out.println(y.doubleValue());
+
+        // Returns long primitive data type
+        System.out.println(y.longValue());
+
+        // CompareTo
+        Integer t = 5;
+
+        System.out.println(t.compareTo(3));
+        System.out.println(t.compareTo(5));
+        System.out.println(t.compareTo(8));
+
+        // equals
+        Integer c = 5;
+        Integer r = 10;
+        Integer l = 5;
+        Short m = 5;
+        System.out.println(c.equals(r));
+        System.out.println(c.equals(l));
+        System.out.println(c.equals(m));
+        //Value
+        Integer x = Integer.valueOf(9);
+        Double f = Double.valueOf(5);
+        Float a = Float.valueOf("80");
+        Integer b = Integer.valueOf("444", 16);
+        // toString
+        Integer p = 5;
+
+        System.out.println(p.toString());
+        System.out.println(Integer.toString(12));
+
+        System.out.println(x);
+        System.out.println(f);
+        System.out.println(a);
+        System.out.println(b);
+        // parseInt
+        int v = Integer.parseInt("9");
+        double i = Double.parseDouble("5");
+        int o = Integer.parseInt("444", 16);
+
+        System.out.println(v);
+        System.out.println(i);
+        System.out.println(o);
+        // abs
+        Integer q = -8;
+        double w = -100;
+        float s = -90;
+
+        double d = -100.675;
+        float g = -90;
+        System.out.println(Math.abs(q));
+        System.out.println(Math.abs(w));
+        System.out.println(Math.abs(s));
+        // ceil and floor
+        System.out.println(Math.ceil(d));
+        System.out.println(Math.ceil(g));
+
+        System.out.println(Math.floor(d));
+        System.out.println(Math.floor(g));
+        // rint
+        double da = 100.675;
+        double ea = 100.500;
+        double fa = 100.200;
+        System.out.println(Math.rint(da));
+        System.out.println(Math.rint(ea));
+        System.out.println(Math.rint(fa));
+
+        // Round
+        double de = 100.675;
+        double ee = 100.500;
+        float fe = 100;
+        float ge = 90f;
+        System.out.println(Math.round(de));
+        System.out.println(Math.round(ee));
+
+        System.out.println(Math.round(fe));
+        System.out.println(Math.round(ge));
+
+        //min
+        System.out.println(Math.min(12.123, 12.456));
+        System.out.println(Math.min(23.12, 23.0));
+
+        //max
+        System.out.println(Math.max(12.123, 12.456));
+        System.out.println(Math.max(23.12, 23.0));
+
+         // random
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        // isLetter
+        System.out.println(Character.isLetter('c'));
+        System.out.println(Character.isLetter('5'));
+        // isDigit
+        System.out.println(Character.isDigit('c'));
+        System.out.println(Character.isDigit('5'));
+        // isWhitespace
+        System.out.println(Character.isWhitespace('c'));
+        System.out.println(Character.isWhitespace(' '));
+        System.out.println(Character.isWhitespace('\n'));
+        System.out.println(Character.isWhitespace('\t'));
+         //isUpperCase
+        System.out.println(Character.isUpperCase('c'));
+        System.out.println(Character.isUpperCase('C'));
+        System.out.println(Character.isUpperCase('\n'));
+        System.out.println(Character.isUpperCase('\t'));
+        //isLowerCase
+        System.out.println(Character.isLowerCase('c'));
+        System.out.println(Character.isLowerCase('C'));
+        System.out.println(Character.isLowerCase('\n'));
+        System.out.println(Character.isLowerCase('\t'));
+        //toUpperCase
+        System.out.println(Character.toUpperCase('c'));
+        System.out.println(Character.toUpperCase('C'));
+        //toLowerCase
+        System.out.println(Character.toLowerCase('c'));
+        System.out.println(Character.toLowerCase('C'));
+        //toString
+        System.out.println(Character.toString('c'));
+        System.out.println(Character.toString('C'));
+       // StringCompareto
+        String str1 = "Strings are immutable";
+        String str2 = new String("Strings are immutable");
+        String str3 = new String("Integers are not immutable");
+
+        int result = str1.compareTo( str2 );
+        System.out.println(result);
+
+        result = str2.compareTo( str3 );
+        System.out.println(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
